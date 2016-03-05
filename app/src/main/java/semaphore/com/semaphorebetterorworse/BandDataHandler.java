bd8b70ab2106dd321a6a941a4e81b526ae5597ad
William <williamstogin2018@u.northwestern.edu>
2016-03-05 16:22:27 -0600

    Added microsoft band api

--- app/src/main/java/semaphore/com/semaphorebetterorworse/BandDataHandler.java
+++ app/src/main/java/semaphore/com/semaphorebetterorworse/BandDataHandler.java
@@ -1,15 +1,22 @@
 package semaphore.com.semaphorebetterorworse;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandInfo;
 
 /**
  * Created by William on 3/5/2016.
  */
 public class BandDataHandler extends AsyncTask<Void,Void,Void> {
-
+    private BandInfo[] pairedBands;
+    private final String TAG = "BandDataHandler";
     @Override
     protected Void doInBackground(Void... params) {
         // Init, get the list of connected mBands
+        pairedBands = BandClientManager.getInstance().getPairedBands();
+
 
         // This is a test
 
@@ -17,6 +24,14 @@ public class BandDataHandler extends AsyncTask<Void,Void,Void> {
 
 
         return null;
+    }
+
+
+    private void printPairedBands() {
+        for (BandInfo info :pairedBands) {
+            Log.v(TAG, "Paired band detected: " + info.getName());
+        }
+
     }
 
 
